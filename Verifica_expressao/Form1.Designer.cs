ea968ecf17e4d9ad12fd60b337cbaeeeed3197f2
Diego Francisco <deliasfrancisco@gmail.com>
2020-04-05 11:35:36 -0300

    #initial commit

---
+++ Verifica_expressao/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Verifica_expressao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.label1 = new System.Windows.Forms.Label();
+			this.TBExpressao = new System.Windows.Forms.TextBox();
+			this.BTProcessa = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.TBResultado = new System.Windows.Forms.TextBox();
+			this.BTLimpar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(29, 48);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Expressão";
+			// 
+			// TBExpressao
+			// 
+			this.TBExpressao.Location = new System.Drawing.Point(100, 41);
+			this.TBExpressao.Name = "TBExpressao";
+			this.TBExpressao.Size = new System.Drawing.Size(100, 20);
+			this.TBExpressao.TabIndex = 1;
+			// 
+			// BTProcessa
+			// 
+			this.BTProcessa.Location = new System.Drawing.Point(100, 87);
+			this.BTProcessa.Name = "BTProcessa";
+			this.BTProcessa.Size = new System.Drawing.Size(100, 23);
+			this.BTProcessa.TabIndex = 2;
+			this.BTProcessa.Text = "Processa";
+			this.BTProcessa.UseVisualStyleBackColor = true;
+			this.BTProcessa.Click += new System.EventHandler(this.BTProcessa_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(29, 180);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Resultado";
+			// 
+			// TBResultado
+			// 
+			this.TBResultado.Enabled = false;
+			this.TBResultado.Location = new System.Drawing.Point(100, 180);
+			this.TBResultado.Name = "TBResultado";
+			this.TBResultado.Size = new System.Drawing.Size(100, 20);
+			this.TBResultado.TabIndex = 4;
+			// 
+			// BTLimpar
+			// 
+			this.BTLimpar.Location = new System.Drawing.Point(100, 138);
+			this.BTLimpar.Name = "BTLimpar";
+			this.BTLimpar.Size = new System.Drawing.Size(100, 23);
+			this.BTLimpar.TabIndex = 5;
+			this.BTLimpar.Text = "Limpar";
+			this.BTLimpar.UseVisualStyleBackColor = true;
+			this.BTLimpar.Click += new System.EventHandler(this.BTLimpar_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(280, 229);
+			this.Controls.Add(this.BTLimpar);
+			this.Controls.Add(this.TBResultado);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.BTProcessa);
+			this.Controls.Add(this.TBExpressao);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Verifica expressão";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBExpressao;
+        private System.Windows.Forms.Button BTProcessa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBResultado;
+		private System.Windows.Forms.Button BTLimpar;
+	}
+}
+
---
+++ Verifica_expressao/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verifica_expressao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Organização dos dados
+        const int MAX = 50;
+
+        //struct pilha
+        struct tp_pilha
+        {
+            public int topo;
+            public char[] vetor;
+        }
+        tp_pilha pilha;
+
+        // Inicialização
+        void Inicializa(ref tp_pilha p)
+        {
+            p.topo = -1;
+            p.vetor = new char[MAX];
+        }
+
+        // Verificando limites
+        bool EstaVazia(tp_pilha p)
+        {
+            return (p.topo == -1);
+        }
+
+        bool EstaCheia(tp_pilha p)
+        {
+            return (p.topo == MAX - 1);
+        }
+
+        // Insere um elemento
+        char Insere(ref tp_pilha p, char letra)
+        {
+            return p.vetor[++p.topo] = letra;
+        }
+
+        // Remove um elemento
+        int Remove(ref tp_pilha p)
+        {
+            return (p.vetor[p.topo--]);
+        }
+
+        //Concatena a string
+        private void Concatena(string resultado)
+        {
+            throw new NotImplementedException();
+        }
+
+        //verifica caracteres
+        bool Verifica(ref tp_pilha p, char[] palavra)
+        {
+            p.vetor = palavra;
+            bool res = true;
+
+            foreach (var item in p.vetor)
+            {
+                if (item.Equals('(') && item.Equals(')') && item.Equals('[') && item.Equals(']') && item.Equals('{') && item.Equals('}'))
+                    res = false;
+            }
+            return res;
+        }
+
+        //Método de evento de click do botão
+        private void BTProcessa_Click(object sender, EventArgs e)
+        {
+            string expressao = TBExpressao.Text;
+            if (string.IsNullOrWhiteSpace(expressao))
+            {
+                TBExpressao.Text = "Vazio";
+                TBResultado.Text = "Vazio";
+            }
+            char[] novaExpressao = new char[expressao.Length];
+            Inicializa(ref pilha);
+
+            for (int i = 0; i < expressao.Length; i++)
+                novaExpressao[i] += Insere(ref pilha, expressao[i]);
+
+            if (Verifica(ref pilha, novaExpressao))
+                for (int i = novaExpressao.Length - 1; i >= 0; i--)
+                    TBResultado.Text += novaExpressao[i].ToString();
+            else
+                TBExpressao.Text = "Caracteres invalidos";
+        }
+
+        private void BTLimpar_Click(object sender, EventArgs e)
+        {
+            TBExpressao.Text = "";
+            TBResultado.Text = "";
+        }
+    }
+}
+
